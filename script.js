6da2a04792630e677ac4b33dafb593bd214f64ed
Shashank88 <shashankreddy1817@gmail.com>
2024-04-03 16:56:33 +0530

    added queries

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let btnEl = document.querySelector(".btn");
 
 let percentage = Math.floor(Math.random() * 100 + 1);
 
-let textB = `<h1>${percentage}% Its better to Stay away!</h1>`;
-let textG = `<h1>${percentage}% Its Ok not a bad relationship!</h1>`;
-let textO = `<h1>${percentage}% Oh, You have a strong relationship!</h1>`;
+let textB = `<h2>${percentage}% Its better to Stay away!</h2>`;
+let textG = `<h2>${percentage}% Its Ok not a bad relationship!</h2>`;
+let textO = `<h2>${percentage}% Oh, You have a strong relationship!</h2>`;
 function getPercentage() {
   if (percentage <= 50) {
     formEl.innerHTML = textB;
